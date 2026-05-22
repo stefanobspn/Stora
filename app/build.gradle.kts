3d33445433f0996af0c38ad3cc1520bab8f7295b
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-05-22 17:58:52 +0700

    chore: add androidx hilt nav compose dependencies and update version some libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.serialization.json)
+    implementation(libs.androidx.hilt.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
